548945ec690963f5ac6b234551bb7bcbc691c675
Mounika <devimonikaallam123@gmail.com>
2026-02-28 11:39:41 +0530

    commit

---
+++ src/main/java/com/example/demo/AwsdeployTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsdeployTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsdeployTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/demoTest.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class demoTest {
+	@Autowired
+	demo c;
+	//demo c = new demo();
+		@Test
+		void csetest() {
+			int result = c.addcse(2,3);
+			assertEquals(5,result);
+		}
+
+
+	}
+	
+
+	
